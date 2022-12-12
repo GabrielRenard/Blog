616a2b318f1a3c152c3e26c255ae41e31470f65b
Gabriel Renard <gabriel.renard@protonmail.com>
2022-12-12 11:17:53 +0100

    add sanity/cli as dependancy

--- sanity-blog/sanity.cli.ts
+++ sanity-blog/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'lpzl7rxq',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
